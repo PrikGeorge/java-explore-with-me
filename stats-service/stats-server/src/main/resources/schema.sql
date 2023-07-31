64b079533abb45e919a0881b992b994c159e639c
George <prikashchenkov@tehnokod.com>
2023-07-31 22:49:48 +0300

    fix

--- stats-service/stats-server/src/main/resources/schema.sql
+++ stats-service/stats-server/src/main/resources/schema.sql
@@ -1,11 +1,10 @@
 DROP TABLE IF EXISTS hits CASCADE;
 
-CREATE TABLE hits (
-    id              INTEGER PRIMARY KEY,
-    app             TEXT,
-    uri             TEXT,
-    ip              TEXT,
-    created         TIMESTAMP WITHOUT TIME ZONE
+CREATE TABLE hits
+(
+    id      SERIAL PRIMARY KEY,
+    app     TEXT,
+    uri     TEXT,
+    ip      TEXT,
+    created TIMESTAMP WITHOUT TIME ZONE
 );
-
-CREATE UNIQUE INDEX hits_idx ON hits (id);
